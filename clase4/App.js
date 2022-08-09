346559c62f3ee4414a88f098b78e039b7bd76966
fernanda rodriguez <fernandarpy@gmail.com>
2022-08-09 15:11:05 -0500

    clase 4

---
+++ clase4/App.js
@@ -0,0 +1,13 @@
+import imagen from './img/imagen_app.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={imagen} alt= "imagen de sujeto feliz"/> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
